7b5d6a5648dd4cb8f9428fafa2e6ca6247de4a1f
DRA <admin>
2018-01-04 19:49:04 +0800

    研究结束2

--- app/src/main/java/com/healthmudi/subjects_home/one/fragment/ResearchEndVisitDetailFragment.java
+++ app/src/main/java/com/healthmudi/subjects_home/one/fragment/ResearchEndVisitDetailFragment.java
@@ -93,6 +93,8 @@ public class ResearchEndVisitDetailFragment extends BaseFragment1 {
             updateEndReason(mSubjectsBean);
         }
 
+
+
         if (mSubjectsPersonalListBean != null) {
             if (TextUtils.isEmpty(mSubjectsPersonalListBean.getRemark())) {
                 mTvRemark.setVisibility(View.GONE);
